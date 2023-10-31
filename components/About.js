ae7980b70a7a3375c8cfda5fadf3a4897f38e368
Abdullah <71959641+Abdullah-washere@users.noreply.github.com>
2023-10-31 06:23:09 +0400

    Update About.js

--- components/About.js
+++ components/About.js
@@ -15,13 +15,14 @@ const About = () => {
       {firstName && (
         <h1 className='about__greeting animate__animated animate__fadeInUp animate__delay-1s'>
           Hey, I&apos;m <span className='text--strong'>{firstName}</span>.
+
           {greetingEmoji && <span> {greetingEmoji}</span>}
         </h1>
       )}
 
       {role && (
         <h1 className='about__role animate__animated animate__fadeInUp animate__delay-3s'>
-          I&aposm a {role}
+          I&apos;m a {role}
           {company && (
             <span>
               {' '}
