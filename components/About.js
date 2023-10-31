89901f574a8231091156431cee052beec645aac0
Abdullah <71959641+Abdullah-washere@users.noreply.github.com>
2023-10-31 06:46:22 +0400

    Update About.js

--- components/About.js
+++ components/About.js
@@ -1,12 +1,13 @@
-import uniqid from 'uniqid'
-import GitHubIcon from '@material-ui/icons/GitHub'
-import LinkedInIcon from '@material-ui/icons/LinkedIn'
-import { about } from '../data/portfolio'
+import React from 'react';
+import uniqid from 'uniqid';
+import GitHubIcon from '@material-ui/icons/GitHub';
+import LinkedInIcon from '@material-ui/icons/LinkedIn';
+import InstagramIcon from '@mui/icons-material/Instagram'; // Import InstagramIcon
+import { about } from '../data/portfolio';
 
 const About = () => {
-  const { name, role, company, description, resume, social, greetingEmoji } =
-    about
-  const firstName = name ? name.split(' ')[0] : null
+  const { name, role, company, description, resume, social, greetingEmoji } = about;
+  const firstName = name ? name.split(' ')[0] : null;
 
   return (
     <div className='about'>
@@ -22,7 +23,7 @@ const About = () => {
 
       {role && (
         <h1 className='about__role animate__animated animate__fadeInUp animate__delay-3s'>
-          I&apos;m a {role}
+          I&aposm a {role}
           {company && (
             <span>
               {' '}
@@ -76,6 +77,18 @@ const About = () => {
                   <LinkedInIcon />
                 </a>
               )}
+
+              {social.instagram && (
+                <a
+                  href={social.instagram} // Replace with your Instagram profile URL
+                  target='_blank'
+                  rel='noreferrer'
+                  aria-label='instagram'
+                  className='link link--icon'
+                >
+                  <InstagramIcon /> {/* Instagram icon */}
+                </a>
+              )}
             </>
           )}
         </div>
@@ -83,7 +96,7 @@ const About = () => {
 
       <div className='about__pseudo-padding' />
     </div>
-  )
+  );
 }
 
-export default About
+export default About;
