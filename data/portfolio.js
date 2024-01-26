9b7940c0a638b646ebe4111db5a56779ee0a0141
Abdullah <71959641+Abdullah-washere@users.noreply.github.com>
2024-01-27 00:56:45 +0400

    Update portfolio.js

--- data/portfolio.js
+++ data/portfolio.js
@@ -1,7 +1,7 @@
 const about = {
   // all the properties are optional - can be left empty or deleted
   // each element in the description array is a paragraph
-  name: 'Abdullah Malik',
+  name: 'Abdullah Maliktr',
   role: 'Web Developer',
   company: 'Stecker Inc',
   description: [
